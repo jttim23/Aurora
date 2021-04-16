5c763827d243d3c3b03fe98e2d070edc236e1906
jedro <jttim23@gmail.com>
2021-04-16 13:05:54 +0200

    dada

--- js/db.js
+++ js/db.js
@@ -1,63 +1,29 @@
-db.collection('events').onSnapshot(snapshot => {
-    // Handle the latest event
-    const newestEvent = snapshot.docChanges()[0].doc.data()
-    const id = snapshot.docChanges()[0].doc.id
-    showLatestEvent(newestEvent, id);
-    
-    // shift the latest event element
-    snapshot.docChanges().shift()
-    
-    snapshot.docChanges().forEach(event => {
-        showEvents(event.doc.data(), event.doc.id)
-    });
-})
 
-const addNewEvent = () => {
-  const event = {
-    name: form.name.value,
-    attendee: form.attendee.value,
-    booked: 0,
-    description: form.description.value,
-    status: parseInt(form.status.value, 10)
-  }
-    db.collection('events').add(event)
-    .then(() => {
-    // Reset the form values
-    form.name.value = "",
-    form.attendee.value = "",
-    form.description.value = "",
-    form.status.value = ""
+var user = firebase.auth().currentUser;
+const addNewReservation = () => {
+  if (user != null) {
+    console.log(user.email);
 
-    alert('Your event has been successfully saved')
-    })
-    .catch(err => console.log(err))
-}
-
-let bookedEvents = [];
-
-const bookEvent = (booked, id) => {
-  const getBookedEvents = localStorage.getItem('booked-events');
-
-    if (getBookedEvents) {
-     bookedEvents = JSON.parse(localStorage.getItem('booked-events'));
-      if(bookedEvents.includes(id)) {
-        alert('Seems like you have already booked this event') 
-      } 
-      else {
-        saveBooking(booked, id)
-     }
-    } 
-    else {
-        saveBooking(booked, id)
+    const reservation = {
+      checkinDate: newBookingform.checkinDate.value,
+      checkoutDate: newBookingform.checkoutDate.value,
+      tentType: newBookingform.tentType.value,
+      phoneNumber: newBookingform.phoneNumber.value,
+      email = user.email,
     }
-};
-
-const saveBooking = (booked, id) => {
-    bookedEvents.push(id);
-    localStorage.setItem('booked-events', JSON.stringify(bookedEvents));
-
-    const data = { booked: booked +1 }
-    db.collection('events').doc(id).update(data)
-    .then(() => alert('Event successfully booked'))
-    .catch(err => console.log(err))
+    db.collection('bookings').add(reservation)
+      .then(() => {
+        // Reset the form values
+        newBookingform.checkinDate.value = "",
+          newBookingform.checkoutDate.value = "",
+          newBookingform.tentType.value = "",
+          newBookingform.phoneNumber.value = "",
+          console.log("send")
+        alert('Your event has been successfully saved')
+      })
+      .catch(err => console.log(err))
+  }
+  if (user == null) {
+    alert("Login First!!!")
+  }
 }
